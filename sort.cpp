fd7ccbadcdfaa6f7bdbb33dd008115dfc90eff8e
Suponji Ayume <503614951@qq.com>
2025-07-13 18:28:54 +0800

    优化排序算法,改成快速排序算法

--- sort.cpp
+++ sort.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 using namespace std;
 
-// 冒泡排序函数
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; ++i) {
-        // 提前退出标志
-        bool swapped = false;
-        for (int j = 0; j < n - 1 - i; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                // 交换相邻元素
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        // 如果本轮没有发生交换，说明已经有序
-        if (!swapped) break;
+// 快速排序函数
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+    int i = left, j = right;
+    int pivot = arr[left];
+    while (i < j) {
+        while (i < j && arr[j] >= pivot) --j;
+        if (i < j) arr[i++] = arr[j];
+        while (i < j && arr[i] <= pivot) ++i;
+        if (i < j) arr[j--] = arr[i];
     }
+    arr[i] = pivot;
+    quickSort(arr, left, i - 1);
+    quickSort(arr, i + 1, right);
 }
 
-// 测试冒泡排序
+// 测试快速排序
 int main() {
     int arr[] = {5, 2, 9, 1, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -30,7 +27,7 @@ int main() {
     }
     cout << endl;
 
-    bubbleSort(arr, n);
+    quickSort(arr, 0, n - 1);
 
     cout << "排序后: ";
     for (int i = 0; i < n; ++i) {
